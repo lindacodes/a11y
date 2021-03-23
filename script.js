9d2d48a763360f7c5a521c4d85cf24a449a6618e
Linda Connolly <45238027+lindacodes@users.noreply.github.com>
2021-03-23 21:24:17 +0000

    Add keyboard shortcuts for quick section switching

--- script.js
+++ script.js
@@ -2,13 +2,32 @@ const currentDate = new Date();
 document.querySelector('footer').innerHTML = `&copy; ${currentDate.getFullYear()} Linda Connolly`;
 
 const buttons = document.querySelectorAll('.accordion button');
+const lastButtonIndex = buttons.length - 1;
 
-buttons.forEach(button => {
+buttons.forEach((button, idx) => {
   button.addEventListener('click', () => {
     const isExpanded = button.getAttribute('aria-expanded') === 'true';
     const accordionPanel = button.parentElement.nextElementSibling;
 
     button.setAttribute('aria-expanded', !isExpanded);
     accordionPanel.classList.toggle('hidden');
-  })
+  });
+
+  button.addEventListener('keydown', (e) => {
+    if (idx < lastButtonIndex && e.key === 'ArrowDown') {
+      buttons[idx + 1].focus();
+    }
+
+    if (idx > 0 && e.key === 'ArrowUp') {
+      buttons[idx - 1].focus();
+    }
+
+    if (e.key === 'Home') {
+      buttons[0].focus();
+    }
+
+    if (e.key === 'End') {
+      buttons[lastButtonIndex].focus();
+    }
+  });
 });
